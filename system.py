cee8f27dbdedcd1ceec1fb023a81390ad1b6642b
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-01-21 01:14:05 +0530

    commit

---
+++ system.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+import io
+import customtkinter
+
+root = customtkinter.CTk()
+root.geometry("1350x750")
+root.title("Book Recommendation System")
+
+
+
+
+
+root.mainloop()
