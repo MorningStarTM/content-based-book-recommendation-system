5eebe5102dc6df0bc8fae495dfaeb41b00c76aa7
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-03-19 00:44:55 +0530

    updated index and style

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
 from flask import Flask, render_template, url_for, Response
-import cv2
+from PIL import Image
 import requests
 import pandas as pd
-import io
+from io import BytesIO
 import numpy as np
 
 app = Flask(__name__)
-
-#function for get image using url
-def get_image():
-    image_list = []
-    df = pd.read_csv('./books')
-    url = df['thumbnail'][1:10]
-    for i in range(10):
-        #get the image by url
-        response = requests.get(url[i])
-        if response.status_code == 200:
-        # Read the image data and convert it to a NumPy array
-            content = np.asarray(bytearray(response.content), dtype=np.uint8)
-            img = content.tobytes()
-            image_list.append(img)
-    return image_list
-
-
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Read image URLs from DataFrame
+    df = pd.read_csv('./books.csv')
+    sorted_df = df.sort_values('average_rating', ascending=False)
+    urls = sorted_df['thumbnail'][0:10].tolist()
+    return render_template('index.html', urls=urls)
 
 
 @app.route('/image')
 def image():
-    return Response(get_image(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+    return Response('index.html')
 
 
 if __name__ == "__main__":
