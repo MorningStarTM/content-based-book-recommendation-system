69b80170e66836ff9994f4f021f6d72ea3ababb3
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-03-23 00:26:47 +0530

    sorted function created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, Response
+from flask import Flask, render_template, url_for, Response, request
 from PIL import Image
 import requests
 import pandas as pd
@@ -15,13 +15,16 @@ def index():
     df = pd.read_csv('./books.csv')
     sorted_df = df.sort_values('average_rating', ascending=False)
     urls = sorted_df['thumbnail'][0:10].tolist()
+    
     return render_template('index.html', urls=urls)
 
 
-@app.route('/image')
+@app.route('/image', methods=('POST', 'GET'))
 def image():
-    
-    return Response('index.html')
+    genre = ''
+    if request.method == 'POST':
+        genre = request.form['name']
+    return render_template('sample.html', h1=genre)
 
 
 if __name__ == "__main__":
