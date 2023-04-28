a429dc32de16f8e5dd056ca87e4dde4041a86d16
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-04-28 23:28:32 +0530

    similarity function created

--- app.py
+++ app.py
@@ -7,37 +7,45 @@ import numpy as np
 
 app = Flask(__name__)
 
+# Read image URLs from DataFrame
+df = pd.read_csv('./books.csv')
 
+#similarity data
+similarity_df = pd.read_csv("./similarity_books.csv")
+
+#find the index of title
+def get_index(title):
+  return df[df.title == title].index.values[0] 
 
 @app.route('/', methods=('POST', 'GET'))
 def index():
-    # Read image URLs from DataFrame
-    df = pd.read_csv('./books.csv')
-    sorted_df = df.sort_values('average_rating', ascending=False)
-    urls = sorted_df['thumbnail'][0:10].tolist()
-    titles = sorted_df['title'][0:10].tolist()
-    
-
-    #get categories of books
-    categories = df['categories'].unique().tolist()
-
+    thumb = []
     #get filtered row and sorting by rating
     if request.method == "POST":
-        category = request.form['name']
+        text = request.form['text']
+        book_id = get_index(text)
+        #find the similar value 
+        similar_books = list(enumerate(similarity_df[str(book_id)]))
+        sorted_books = sorted(similar_books,key=lambda x:x[1],reverse=True)
+        #append similarity books
+        for i in range(20):
+            thumb.append(df[df.index == sorted_books[i][0]]["thumbnail"].values[0])
+        """category = request.form['name']
         df_filtered = df[df['categories'] == category]
         df_filtered = df_filtered.sort_values('average_rating', ascending=False)
         urls = df_filtered['thumbnail'][0:10].tolist()
-        titles = df_filtered['title'][0:10].tolist()
-
-    return render_template('index.html', urls=urls, categories=categories, titles=titles)
+        titles = df_filtered['title'][0:10].tolist()"""
+    
+    return render_template('index.html', thumb=thumb)#, categories=categories, titles=titles)
 
 
 @app.route('/image', methods=('POST', 'GET'))
 def image():
-    genre = ''
-    if request.method == 'POST':
-        genre = request.form['name']
-    return render_template('sample.html', h1=genre)
+    text = ''
+    if request.method == "POST":
+        text = request.form['text']
+        print(text)
+    return render_template('sample.html', h1=text)
 
 
 if __name__ == "__main__":
